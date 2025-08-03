716e0758ee2cd2e697a11204215eee99e027051d
DeweQ <thedeweq@yandex.ru>
2025-08-03 18:47:23 +0300

    Add buttons to remove entered number

--- script.js
+++ script.js
@@ -5,9 +5,42 @@ const controls = document.querySelector("#controls");
 
 buttons.addEventListener("click", e => {
   if (e.target.classList.contains("num"))
-    expression.textContent += e.target.textContent;
+      appendToCurrentOperand(e.target.textContent);
 });
+
+buttons.addEventListener("click", e => {
+  if (e.target.classList.contains("point")) 
+    if (!getCurrentOperand().value.includes("."))
+      appendToCurrentOperand(".");
+  
+});
+
+buttons.addEventListener("click", e => updateDisplayExpression());
+
 controls.addEventListener("click", e => {
-  if (e.target.id === "clear")
-  expression.textContent = "";
-})
+  if (e.target.id === "clear") {
+  leftOperand.value ="";
+  operator="";
+  rightOperand.value="";
+  }
+  if (e.target.id ==="backspace") {
+    let s = getCurrentOperand();
+    s.value = s.value.slice(0,-1);
+  }
+  updateDisplayExpression();
+});
+
+function updateDisplayExpression() {
+  expression.textContent = leftOperand.value+operator+rightOperand.value;
+}
+
+function appendToCurrentOperand(text) {
+  getCurrentOperand().value += text;
+}
+
+function getCurrentOperand () {
+  return operator === ""?leftOperand:rightOperand;
+}
+let leftOperand = {value:""};
+let rightOperand = {value:""};
+let operator = "";
