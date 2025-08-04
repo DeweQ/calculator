fec02751afc6092c3bf9b70d18220d5558f19e62
DeweQ <thedeweq@yandex.ru>
2025-08-04 07:23:01 +0300

    Add expression evaluation

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const expression = document.querySelector("#expression");
 const result = document.querySelector("#result");
 const buttons = document.querySelector("#buttons");
 const controls = document.querySelector("#controls");
+const operators = document.querySelector("#operators")
+const evaluate = document.querySelector(".evaluate");
 
 buttons.addEventListener("click", e => {
   if (e.target.classList.contains("num"))
@@ -12,26 +14,42 @@ buttons.addEventListener("click", e => {
   if (e.target.classList.contains("point")) 
     if (!getCurrentOperand().value.includes("."))
       appendToCurrentOperand(".");
-  
 });
-
-buttons.addEventListener("click", e => updateDisplayExpression());
 
 controls.addEventListener("click", e => {
   if (e.target.id === "clear") {
   leftOperand.value ="";
   operator="";
   rightOperand.value="";
+  res = "";
   }
   if (e.target.id ==="backspace") {
     let s = getCurrentOperand();
     s.value = s.value.slice(0,-1);
   }
-  updateDisplayExpression();
 });
 
-function updateDisplayExpression() {
+operators.addEventListener("click", e => {
+  if (e.target.classList.contains("operator")) {
+    if (operator === "") {
+      operator = e.target.dataset.operator;
+    }
+  }
+});
+
+evaluate.addEventListener("click", e => {
+  if (leftOperand.value != "" &&
+  rightOperand.value != "" &&
+  operator != "") {
+    evaluateExpression();
+  }
+});
+
+document.querySelector("#container").addEventListener("click", e => updateDisplay());
+
+function updateDisplay() {
   expression.textContent = leftOperand.value+operator+rightOperand.value;
+  result.textContent = res;
 }
 
 function appendToCurrentOperand(text) {
@@ -41,6 +59,27 @@ function appendToCurrentOperand(text) {
 function getCurrentOperand () {
   return operator === ""?leftOperand:rightOperand;
 }
+
+function evaluateExpression() {
+  let left = leftOperand.value.includes(".")?parseFloat(leftOperand.value):parseInt(leftOperand.value);
+  let right = rightOperand.value.includes(".")?parseFloat(rightOperand.value):parseInt(rightOperand.value);
+  switch (operator){
+    case "+":
+      res = String(left+right);
+      break;
+      case "-":
+        res =String(left-right);
+      break;
+      case "*":
+        res = String(left*right);
+      break;
+      case "/":
+        res = String(left/right);
+      break;
+  }
+}
+
 let leftOperand = {value:""};
 let rightOperand = {value:""};
 let operator = "";
+let res ="";
