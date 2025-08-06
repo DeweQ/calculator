4c5897b081bb24e90ebf256f3b02e02a92a46e3d
DeweQ <thedeweq@yandex.ru>
2025-08-06 21:23:51 +0300

    Move anonymous functions to named event handlers

--- script.js
+++ script.js
@@ -1,60 +1,62 @@
-// const expression = document.querySelector("#expression");
-// const result = document.querySelector("#result");
 const buttons = document.querySelector("#buttons");
 const controls = document.querySelector("#controls");
 const operators = document.querySelector("#operators")
 const evaluate = document.querySelector(".evaluate");
 
+//#region Event Listeners
 buttons.addEventListener("click", e => {
-  if (e.target.classList.contains("num")) {
-    if (res != "") res = "";
-    appendToCurrentOperand(e.target.textContent);
-  }
-});
+  if (e.target.classList.contains("num"))
+    inputDigit(e.target.textContent);
 
-buttons.addEventListener("click", e => {
   if (e.target.classList.contains("point"))
-    if (!getCurrentOperand().value.includes("."))
-      appendToCurrentOperand(".");
+    inputPoint();
 });
 
 controls.addEventListener("click", e => {
   if (e.target.id === "clear")
     clear();
-  if (e.target.id === "backspace") {
-    let s = getCurrentOperand();
-    s.value = s.value.slice(0, -1);
-  }
+
+  if (e.target.id === "backspace")
+    removeDigit();
 });
 
 operators.addEventListener("click", e => {
-  if (e.target.classList.contains("operator")) {
-    if (res != "") {
-      appendToCurrentOperand(res);
-      res = "";
-    }
-    if (operator === "" ||
-      (operator != "" && rightOperand.value === "")) {
-      operator = e.target.dataset.operator;
-    }
-    if (rightOperand.value != "") {
-      evaluateExpression();
-      appendToCurrentOperand(res);
-      res = "";
-      operator = e.target.dataset.operator;
-    }
-  }
+  if (e.target.classList.contains("operator"))
+    inputOperator(e.target.dataset.operator)
 });
 
-evaluate.addEventListener("click", e => {
-  if (leftOperand.value != "" &&
-    rightOperand.value != "" &&
-    operator != "") {
-    evaluateExpression();
-  }
-});
+evaluate.addEventListener("click", evaluateExpression);
 
 document.querySelector("#container").addEventListener("click", e => updateDisplay());
+//#endregion
+
+//#region Event Handlers
+function inputDigit(digit) {
+  if (res != "") res = "";
+  appendToCurrentOperand(digit);
+}
+
+function inputPoint() {
+  if (!getCurrentOperand().value.includes("."))
+    appendToCurrentOperand(".");
+}
+
+function inputOperator(op) {
+  if (res != "") {
+    appendToCurrentOperand(res);
+    res = "";
+  }
+  if (operator === "" ||
+    (operator != "" && rightOperand.value === "")) {
+    operator = op;
+  }
+  if (rightOperand.value != "") {
+    evaluateExpression();
+    appendToCurrentOperand(res);
+    res = "";
+    operator = op;
+  }
+}
 
 function clear() {
   leftOperand.value = "";
@@ -63,47 +65,56 @@ function clear() {
   res = "";
 }
 
+function removeDigit() {
+  let s = getCurrentOperand();
+  s.value = s.value.slice(0, -1);
+}
+
 function updateDisplay() {
   display.textContent = leftOperand.value + operator + rightOperand.value + res;
 }
 
+function evaluateExpression() {
+  if (leftOperand.value != "" &&
+    rightOperand.value != "" &&
+    operator != "") {
+    let left = leftOperand.value.includes(".") ? parseFloat(leftOperand.value) : parseInt(leftOperand.value);
+    let right = rightOperand.value.includes(".") ? parseFloat(rightOperand.value) : parseInt(rightOperand.value);
+    switch (operator) {
+      case "+":
+        res = String(left + right);
+        break;
+      case "-":
+        res = String(left - right);
+        break;
+      case "*":
+        res = String(left * right);
+        break;
+      case "/":
+        res = String(left / right);
+        break;
+    }
+    if (!Number.isFinite(parseInt(res))) {
+      alert("The MATH GODS banned me from doing this");
+      clear();
+      return;
+    }
+    leftOperand.value = "";
+    rightOperand.value = "";
+    operator = "";
+  }
+}
+//#endregion
+
 function appendToCurrentOperand(text) {
   if (text.length >= 10)
-    text = text.slice(0,9);
+    text = text.slice(0, 9);
   if (getCurrentOperand().value.length + text.length < 10)
     getCurrentOperand().value += text;
 }
 
 function getCurrentOperand() {
   return operator === "" ? leftOperand : rightOperand;
-}
-
-function evaluateExpression() {
-  let left = leftOperand.value.includes(".") ? parseFloat(leftOperand.value) : parseInt(leftOperand.value);
-  let right = rightOperand.value.includes(".") ? parseFloat(rightOperand.value) : parseInt(rightOperand.value);
-  switch (operator) {
-    case "+":
-      res = String(left + right);
-      break;
-    case "-":
-      res = String(left - right);
-      break;
-    case "*":
-      res = String(left * right);
-      break;
-    case "/":
-      res = String(left / right);
-      break;
-  }
-  if (!Number.isFinite(parseInt(res))) {
-    alert("The MATH GODS banned me from doing this");
-    clear();
-    return;
-  }
-  leftOperand.value = "";
-  rightOperand.value = "";
-  operator = "";
-
 }
 
 let leftOperand = { value: "" };
